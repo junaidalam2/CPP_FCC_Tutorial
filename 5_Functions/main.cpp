bd02afc02528d94f2df5e616df69e07ae3f8e839
junaidalam2 <junaid.alam2@gmail.com>
2025-11-11 08:38:35 -0800

    2:31

---
+++ 5_Functions/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int addNumbers(int first_param, int second_param) {
+
+    int result = first_param + second_param;
+
+    return result;
+}
+
+
+
+
+int main() {
+    
+    int first_number {3};
+    int second_number {7};
+
+    std::cout << "First nummber: " << first_number << std::endl;
+    std::cout << "Second number: " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+
+    std::cout << "Sum: " << addNumbers(first_number, sum) << std::endl;
+
+    return 0;
+
+}
+
