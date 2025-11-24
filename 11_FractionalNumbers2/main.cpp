e07d4a035553ba0a9fe94d98ea5eb0e977c6e29f
junaidalam2 <junaid.alam2@gmail.com>
2025-11-23 23:30:36 -0800

    minor change

--- 11_FractionalNumbers2/main.cpp
+++ 11_FractionalNumbers2/main.cpp
@@ -4,7 +4,7 @@ int main() {
     
     float number1 {1.2345666666544f};
     double number1 {1.2345666666544};
-    long double number1 {1.2345666666544};
+    long double number1 {1.234566666654};
     
     return 0;
 }
